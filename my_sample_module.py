0d8da5410ae7bc81dbe45e9e43d51f0bbf3fabae
Robert_Mieth <robert.mieth@ieee.org>
2018-01-11 15:10:34 -0500

    added julia snippet

--- my_sample_module.py
+++ my_sample_module.py
@@ -1,3 +1,18 @@
+import subprocess
+
 def sample_function(n):
     print("You entered {}".format(n))
+
+    args = ["julia", "julia/the_jul_mod.jl"]
+
+    with open('julia/julia.log', 'w') as log:
+        with subprocess.Popen(args, shell=True, stdout=subprocess.PIPE) as programm:
+            for line in programm.stdout:
+                log.write(line.decode())
+                print(line.decode(), end='')
+
     return True
+
+
+if __name__ == "__main__":
+    sample_function(5)
